e215251135dd99c4c54bf19947df2c1e094a148c
TazhibTechology <tazhibtechnology@gmail.com>
2024-12-01 11:23:04 +0430

    Create Route Appointment App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
 import {Route, Routes} from 'react-router-dom'
-import Home from './pages/Home'
+import {Home, Doctors, About, Contact, Login, MyAppointment, Appointment,MyProfile} from './pages/'
+
 
 const App = () => {
   return (
     <div>
       <Routes>
         <Route path='/' element={<Home/>}></Route>
+        <Route path='/doctors' element={<Doctors/>}></Route>
+        <Route path='/doctors/:speciality' element={<Doctors/>}></Route>
+        <Route path='/login' element={<Login/>}></Route>
+        <Route path='/about' element={<About/>}></Route>
+        <Route path='/contact' element={<Contact/>}></Route>
+        <Route path='/my_profile' element={<MyProfile/>}></Route>
+        <Route path='/my_appiontment' element={<MyAppointment/>}></Route>
+        <Route path='/appiontment/:docID' element={<Appointment/>}></Route>
       </Routes>
     </div>
   )
