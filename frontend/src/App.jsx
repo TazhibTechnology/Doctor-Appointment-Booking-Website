5dd186211aabc1a6d9cc8f434a92f11ebea419cc
TazhibTechology <tazhibtechnology@gmail.com>
2024-12-01 12:41:17 +0430

    fix router error

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,19 @@
-import {Route, Routes} from 'react-router-dom'
-import {Home, Doctors, About, Contact, Login, MyAppointment, Appointment,MyProfile} from './pages/'
+import {Route, Routes} from 'react-router-dom';
+import Home from './pages/Home';
+import Doctors from './pages/Doctors';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Login from './pages/Login';
+import MyAppointments from './pages/MyAppointments';
+import Appointment from './pages/Appointment';
+import MyProfile from './pages/MyProfile';
+import Navbar from './components/navbar'
 
 
 const App = () => {
   return (
     <div>
+      <Navbar/>
       <Routes>
         <Route path='/' element={<Home/>}></Route>
         <Route path='/doctors' element={<Doctors/>}></Route>
@@ -13,8 +22,8 @@ const App = () => {
         <Route path='/about' element={<About/>}></Route>
         <Route path='/contact' element={<Contact/>}></Route>
         <Route path='/my_profile' element={<MyProfile/>}></Route>
-        <Route path='/my_appiontment' element={<MyAppointment/>}></Route>
-        <Route path='/appiontment/:docID' element={<Appointment/>}></Route>
+        <Route path='/my_appointment' element={<MyAppointments/>}></Route>
+        <Route path='/appointment/:docID' element={<Appointment/>}></Route>
       </Routes>
     </div>
   )
