42c3b8605722fd3ffd97506782ae99bea829de58
TazhibTechology <tazhibtechnology@gmail.com>
2024-12-08 20:32:05 +0430

    div appointment

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -1,27 +1,49 @@
-import React, { useContext, useEffect, useState } from 'react'
-import {useParams} from 'react-router-dom'
-import {AppContext} from '../context/AppContext'
+import React, { useContext, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { AppContext } from "../context/AppContext";
+import { assets } from "../assets/assets_frontend/assets";
 
 const Appointment = () => {
+  const { docId } = useParams();
+  const { doctors } = useContext(AppContext);
 
-  const {docId} = useParams()
-  const {doctors} = useContext(AppContext)
+  const [docInfo, setDocInfo] = useState(null);
 
-  const [docInfo, setDocInfo] = useState(null)
-
-  const fetchDocInfo = async() =>{
-    const docInfo = doctors.find(doc => doc._id === docId)
-    setDocInfo(docInfo)
+  const fetchDocInfo = async () => {
+    const docInfo = doctors.find((doc) => doc._id === docId);
+    setDocInfo(docInfo);
     // console.log(docInfo)
-  }
+  };
 
-  useEffect(()=>{
-    fetchDocInfo()
-  },[doctors,docId])
+  useEffect(() => {
+    fetchDocInfo();
+  }, [doctors, docId]);
 
-  return (
-    <div>Appointment</div>
-  )
-}
+  return docInfo && (
+      <div>
+        {/* Doctor Details */}
+        <div>
+          <div>
+            <img src={docInfo.image} alt='' />
+          </div>
+        </div>
+        <div>
+          {/* Doc info name degree exper */}
+          <p>
+            {docInfo.name} 
+            <img src={assets.verified_icon} alt='' />
+          </p>
+          <div>
+            <p>{docInfo.degree} - {docInfo.speciality}</p>
+            <button>{docInfo.experience}</button>
+          </div>
+          {/* doctor About */}
+          <div></div>
 
-export default Appointment
+        </div>
+      </div>
+    
+  );
+};
+
+export default Appointment;
