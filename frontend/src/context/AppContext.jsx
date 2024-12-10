f927428aaa920d021e549d860920aa9cf96f10f6
TazhibTechology <tazhibtechnology@gmail.com>
2024-12-10 09:28:22 +0430

    Add style appointment section1

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -1,18 +1,19 @@
 import { createContext } from "react";
 import { doctors } from "../assets/assets_frontend/assets";
 
-export const AppContext = createContext()
+export const AppContext = createContext();
 
-const AppContextProvider =(props)=>{
-    const value ={
-        doctors
-    }
+const AppContextProvider = (props) => {
+  const currencySymbol = "$";
 
-    return (
-        <AppContext.Provider value={value}>
-            {props.children}
-        </AppContext.Provider>
-    )
-}
+  const value = {
+    doctors,
+    currencySymbol,
+  };
 
-export default AppContextProvider
+  return (
+    <AppContext.Provider value={value}>{props.children}</AppContext.Provider>
+  );
+};
+
+export default AppContextProvider;
