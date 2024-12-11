31c345813d4f52cd45330bb5dc83717f3bb0b884
TazhibTechology <tazhibtechnology@gmail.com>
2024-12-11 09:23:19 +0430

    dev complete dayOfWeek time and hour

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -127,7 +127,9 @@ const Appointment = () => {
         </div>
         {/* Booking slots */}
         <div className='sm:ml-72 sm:pl-4 mt-4  font-medium text-gray-700'>
+
           <p>Booking slots</p>
+
           <div className='flex gap-3 items-center w-full overflow-x-scroll mt-4'>
             {docSlots.length &&
               docSlots.map((item, index) => (
@@ -145,7 +147,24 @@ const Appointment = () => {
                 </div>
               ))}
           </div>
-          <div></div>
+
+          <div className='flex items-center gap-3 w-full overflow-x-scroll mt-4'>
+            {docSlots.length &&
+              docSlots[slotIndex].map((item, index) => (
+                <p
+                  onClick={() => setSlotTime(item.time)}
+                  className={`text-sm font-light flex-shrink-0 px-5 py-2 rounded-full cursor-pointer ${
+                    item.time === slotTime
+                      ? "bg-primary text-gray-100 border"
+                      : "border-gray-300"
+                  }`}
+                  key={index}
+                >
+                  {item.time.toLowerCase()}
+                </p>
+              ))}
+          </div>
+          <button className="bg-primary text-white text-sm font-light my-6 px-14 py-3 rounded-full">Book an appointment</button>
         </div>
       </div>
     )
