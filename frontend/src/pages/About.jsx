a81fd49d6dd080800aba9c992cca65cc56968df7
TazhibTechology <tazhibtechnology@gmail.com>
2024-12-12 20:05:28 +0430

    check my code and try fix

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -44,23 +44,21 @@ const About = () => {
           WHY <span className='text-gray-700 font-semibold'>CHOOSE US</span>
         </p>
       </div>
-      <div className="flex flex-col md:flex-row mb-20">
-        <div className="border px-10 md:px-16  py-8 sm:py16 flex flex-col gap-5 text-[15px] hover: bg-primary hover:text-white transition-all duration-300 text-gray-600">
+      {/* <div className='flex flex-col md:flex-row mb-20'> */}
+        <div className='border px-10 md:px-16  py-8 sm:py16 flex flex-col gap-5 text-[15px] hover: bg-primary hover:text-white transition-all duration-300 text-gray-600'>
           <b>EFFICIENCY:</b>
           <p>
             Streomlined Appointment Scheduling That Fits Into Your Busy
             Lifestyle.
           </p>
         </div>
-        <div className="border px-10 md:px-16  py-8 sm:py16 flex flex-col gap-5 text-[15px] hover: bg-primary hover:text-white transition-all duration-300 text-gray-600">
-          
+        <div className='border px-10 md:px-16  py-8 sm:py16 flex flex-col gap-5 text-[15px] hover: bg-primary hover:text-white transition-all duration-300 text-gray-600'>
           <b>CONVENIENCE:</b>
           <p>
             Access To A Trenrar a frusted Healthcare Professionols In Your Area.
           </p>
         </div>
-        <div className="border px-10 md:px-16  py-8 sm:py16 flex flex-col gap-5 text-[15px] hover: bg-primary hover:text-white transition-all duration-300 text-gray-600">
-          
+        <div className='border px-10 md:px-16  py-8 sm:py16 flex flex-col gap-5 text-[15px] hover: bg-primary hover:text-white transition-all duration-300 text-gray-600'>
           <b>PERSONALIZATION:</b>
           <p>
             Tailered Recommendations And Reminders To Help You Stay On Top Of
