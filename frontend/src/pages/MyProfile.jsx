0e05453f5b5e6378fff4a34933d3a505604c8037
TazhibTechology <tazhibtechnology@gmail.com>
2024-12-15 07:16:51 +0430

    dev myprofile

--- frontend/src/pages/MyProfile.jsx
+++ frontend/src/pages/MyProfile.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {assets} from '../assets/assets_frontend/assets'
 
 const MyProfile = () => {
+  const [userData, setUserData] = useState({
+    name: 'Bilal Ahmad',
+    image:assets.profile_pic,
+    email: 'eng.bilalahmad@gmail.com',
+    phone: '+93 799 1534 19',
+    address:{
+      line1:'Afghanistan, Herat',
+      line2: 'Afghanistan, Kabul'
+    },
+    gender: 'Male',
+    dob: '2005-12-16'
+  })
+  const [isEdit, setIsEdit] = useState(false)
   return (
-    <div>MyProfile</div>
+    <div>
+      <img src={userData.image} alt="" />
+      {
+        isEdit? <input type="text" value={userData.name} onChange={e=>setUserData(prev=>({...prev,name:e.target.value}))}/>:<p>{userData.name}</p>
+      }
+
+    </div>
   )
 }
 
